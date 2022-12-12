4084ae04f2cdbe0bcf752514070d3f42dfd70611
Kishor Kumar Bhaumik <kishorebd2500@gmail.com>
2022-12-12 17:14:56 +0900

    Update layer.py

--- layer.py
+++ layer.py
@@ -26,13 +26,7 @@ class STGRU(nn.Module):
         self.dp = nn.Dropout(p=dp)
 
         self.gcnglu = gcn_glu(gcn_in_channel, gcn_in_channel)
-        #self.gcnglu2 = gcn_glu(gcn_in_channel, gcn_in_channel)
         self.lk = nn.LeakyReLU(0.1)
-
-        #self.conv1d1 = nn.Conv1d(2*in_features, 2*in_features, kernel_size=1 )
-        # self.conv1d2 = nn.Conv1d(3*in_features, 3*in_features, kernel_size=1 )
-        # self.mlean1 = nn.Linear(gcn_in_channel,gcn_in_channel)
-        # self.mlean2 = nn.Linear(gcn_in_channel,gcn_in_channel)
 
         self.ap =nn.AvgPool1d(kernel_size=2)
 
@@ -45,8 +39,6 @@ class STGRU(nn.Module):
 
         zh =x
         x = self.lk(self.gcnglu(x.permute(1,0,2),A).permute(1,0,2)) 
-        # x+=zh
-        # x = self.lk(self.gcnglu(x.permute(1,0,2),A).permute(1,0,2)) 
 
         ## attentive attention
         C = torch.cat((mem,x),1)
@@ -58,20 +50,10 @@ class STGRU(nn.Module):
         a2= a2*mem
         zm = a1+a2
         y= torch.cat((x,zm),1)
-        #pdb.set_trace()
-        #y = self.conv1d1(y)
         y=torch.cat((y,zh),1)
-        #y= self.conv1d2(y)
+
         wh, wr, wz = torch.split(y, self.in_features, dim = 1)
 
         mem = self.GRU(wh,wr,wz, mem)
-        # mem = self.dp(mem)
-        # mo, mg, mi = torch.split(y, self.in_features, dim = 1)
-
-        # mi = torch.sigmoid(mi)
-        # new_m = (1 - mi) * mem + mi * torch.tanh(mg)
-        # new_h = torch.sigmoid(mo) * new_m 
-        
-        # return  new_h, new_m 
 
         return  mem
